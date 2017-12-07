f9a2f35146b714b1f4da951be0cdddb71e07f6ab
John Tuttle <thejwt@gmail.com>
2017-12-07 02:46:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ function fatBastard(meal) {
 
 function raspyDoorGuy() {
   const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
- 
+
   function givePassword(givenPassword) { // givePassword() is the inner function, a closure
-    if (givenPassword === password) { // givePassword() uses variable declared in the parent function 
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
       console.log('Ye may enter.');
     } else {
       console.log('Begone, landlubber!');
     }
   }
- 
+
   return {
     givePassword
   };
