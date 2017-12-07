bcb19ca32241ba64fd1ab867c018ded752f38b9f
John Tuttle <thejwt@gmail.com>
2017-12-07 02:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 function fatBastard(meal) {
   function whatsForDinner() { // whatsForDinner() is an inner function, a closure
-    if (!meal) { // whatsForDinner() uses argument provided to the parent function 
+    if (!meal) { // whatsForDinner() uses argument provided to the parent function
       console.log('My belly is empty. Woe is me.');
     } else if (meal === 'Mini-Me') {
       console.log('The wee man is in my belly!');
@@ -10,13 +10,29 @@ function fatBastard(meal) {
       console.log(`I'm eatin' a bit of ${meal}! Burp.`);
     }
   }
- 
+
   function digest() { // digest() is an inner function, a closure
-    meal = undefined; // digest() uses argument provided to the parent function 
+    meal = undefined; // digest() uses argument provided to the parent function
   }
- 
+
   return {
     whatsForDinner,
     digest
+  };
+}
+
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+ 
+  function givePassword(givenPassword) { // givePassword() is the inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function 
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+ 
+  return {
+    givePassword
   };
 }
